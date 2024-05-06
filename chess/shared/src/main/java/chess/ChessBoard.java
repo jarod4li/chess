a035a5b9bb0f4f6af874f453a4a7604f48e17975
jarod4li <105070775+jarod4li@users.noreply.github.com>
2024-05-06 00:31:53 -0600

    fix(shared) maven resource reference

--- chess/shared/src/main/java/chess/ChessBoard.java
+++ chess/shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,5 @@
 package chess;
-
+import java.util.Arrays;
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,9 +7,18 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    private ChessPiece[][] squares=new ChessPiece[8][8];
 
     public ChessBoard() {
-        
+
+    }
+
+    public ChessBoard(ChessBoard board) {
+        for (int i=0; i < 8; i++) {
+            for (int j=0; j < 8; j++) {
+                this.squares[i][j]=board.getPiece(new ChessPosition(i + 1, j + 1));
+            }
+        }
     }
 
     /**
@@ -19,7 +28,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        squares[position.getRow() - 1][position.getColumn() - 1]=piece;
     }
 
     /**
@@ -30,7 +39,20 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return squares[position.getRow() - 1][position.getColumn() - 1];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that=(ChessBoard) o;
+        return Arrays.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(squares);
     }
 
     /**
