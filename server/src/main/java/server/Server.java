46ed330d9eb76cd7defa2b77144ad67dd32fe14c
jarod4li <105070775+jarod4li@users.noreply.github.com>
2024-07-23 09:28:15 -0600

    added files and started server

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,26 @@
+package server;
+
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+        Spark.delete("/name/:name", this::deleteName);
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+    private Object deleteName(Request req, Response res) {
+        return 0;
+    }
+}
