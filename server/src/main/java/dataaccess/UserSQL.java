3d42b9a4536c87b9e38a109c2ee09c7b5df6f6ae
jarod4li <105070775+jarod4li@users.noreply.github.com>
2025-10-30 22:58:16 -0600

    UserSQL added

---
+++ server/src/main/java/dataaccess/UserSQL.java
@@ -0,0 +1,69 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserSQL implements UserDAO {
+    public UserSQL() throws DataAccessException  {
+        configureDatabase();
+    }
+    private static final String[] USERS_TABLE_QUERY = {
+            "CREATE TABLE IF NOT EXISTS users (" +
+                    "username VARCHAR(50) NOT NULL, " +
+                    "password VARCHAR(255) NOT NULL, " +
+                    "email VARCHAR(100) NOT NULL, " +
+                    "PRIMARY KEY (username)" +
+                    ")"
+    };
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String query : USERS_TABLE_QUERY) {
+                try (PreparedStatement statement = conn.prepareStatement(query)) {
+                    statement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public UserData getUserWithUsername(String username) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement statement = conn.prepareStatement("SELECT * FROM users WHERE username = ?")) {
+            statement.setString(1, username);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    String fetchedUsername = resultSet.getString("username");
+                    String password = resultSet.getString("password");
+                    String email = resultSet.getString("email");
+                    return new UserData(fetchedUsername, password, email);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: " + e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public UserData getUserWithEmail(String email) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void addUser(String username, String password, String email) throws DataAccessException {
+
+    }
+
+    @Override
+    public void clearAllUsers() throws DataAccessException {
+
+    }
+}
