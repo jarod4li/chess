0a39d8d8b731d8c856099d77fb6f1848425eac09
jarod4li <105070775+jarod4li@users.noreply.github.com>
2025-11-16 21:02:14 -0700

    login

--- client/src/main/java/ui/PostLogin.java
+++ client/src/main/java/ui/PostLogin.java
@@ -1,19 +1,74 @@
 package ui;
 
-import chess.ChessGame;
-import model.GameData;
-
 import java.util.Scanner;
 
 public class PostLogin {
     private final static Scanner scanner = new Scanner(System.in);
-    private static ChessGame.TeamColor teamColor;
-    private static ServerFacade serverFacade;
+    private final static ServerFacade serverFacade = new ServerFacade();
 
-    public static void postLogin(String token, Integer portNumber, ServerFacade serverFacade) {
-    }
 
-    public static ServerFacade getServerFacade() {
-        return null;
+    public static void postLogin(String token){
+        String gameName;
+        String gameID;
+        String playerColor;
+
+        System.out.println("\nOptions:");
+        System.out.println("Create game");
+        System.out.println("Join game");
+        System.out.println("List games");
+        System.out.println("Help");
+        System.out.println("Logout");
+
+        while (true){
+            String input = scanner.nextLine().toLowerCase();
+            switch (input) {
+                case "create game" -> {
+                    System.out.println("\nEnter game name:");
+                    gameName = scanner.nextLine();
+                    gameID = serverFacade.createGame(gameName, token);
+                    if (gameID != null) {
+                        postLogin(token);
+                    }
+                }
+                case "join game" -> {
+                    System.out.println("Enter game ID:");
+                    gameID = scanner.nextLine();
+                    System.out.println("Join white, black, or none?");
+                    playerColor = scanner.next().toLowerCase();
+                    if (playerColor.equals("none")) {
+                        playerColor = null;
+                    }
+                    while (!(playerColor.equals("white") || playerColor.equals("black"))) {
+                        System.out.println("Please choose white, black, or none.");
+                        playerColor = scanner.next().toLowerCase();
+                        if (playerColor.equals("none")) {
+                            playerColor = null;
+                            break;
+                        }
+                    }
+                    boolean joined = serverFacade.joinGame(gameID, playerColor, token);
+                    if (joined) {
+                        GameConsole.runGame();
+                    } else {
+                        postLogin(token);
+                    }
+                }
+                case "list games" -> {
+                    serverFacade.listAllGames(token);
+                    postLogin(token);
+                }
+                case "help" -> {
+                    System.out.println("Type one of the options.");
+                    postLogin(token);
+                }
+                case "logout" -> {
+                    serverFacade.logout(token);
+                    PreLogin.preLogin();
+                }
+                default -> System.out.println("Invalid input! ");
+            }
+        }
+
+
     }
 }
--- client/src/main/java/ui/PreLogin.java
+++ client/src/main/java/ui/PreLogin.java
@@ -4,9 +4,67 @@ import java.util.Scanner;
 
 public class PreLogin {
     private static Scanner scanner = new Scanner(System.in);
-    private static ServerFacade serverFacade = null;
-    private static int portNumber;
+    private static ServerFacade facade = new ServerFacade();
 
-    public static void preLogin(int portNumber) {
+    public static void preLogin(){
+        String username;
+        String password;
+        String email;
+        String token;
+
+        System.out.println("Chess Game CS240");
+        while(true){
+            System.out.println("Choose one of the options:");
+            System.out.println("Register");
+            System.out.println("Login");
+            System.out.println("Help");
+            System.out.println("Quit");
+
+            String input = scanner.nextLine().toLowerCase().trim();
+            switch (input){
+                case "register":
+                    System.out.print("Username: ");
+                    username = scanner.nextLine().trim();
+                    System.out.print("Password: ");
+                    password = scanner.nextLine().trim();
+                    System.out.print("email: ");
+                    email = scanner.nextLine().trim();
+
+                    token = facade.register(username, password, email);
+
+                    if (token != null){
+                        System.out.println("\nRegistration success! Welcome " + username);
+                        PostLogin.postLogin(token);
+                        break;
+                    }
+
+                case "login":
+                    System.out.print("Username: ");
+                    username = scanner.nextLine().trim();
+                    System.out.print("Password: ");
+                    password = scanner.nextLine().trim();
+
+                    token = facade.login(username, password);
+
+                    if (token != null){
+                        System.out.println("\nLogin success! Welcome " + username);
+                    }
+                    else{
+                        System.out.println("No success :(");
+                    }
+                    PostLogin.postLogin(token);
+
+                    break;
+                case "help":
+                    System.out.println("Select one of the given options and provide the requested information.");
+                    break;
+                case "quit":
+                    System.out.println("Already leaving? Okay :*");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid option.");
+            }
+        }
     }
 }
